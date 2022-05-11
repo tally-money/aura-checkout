c44a665578885cbc4efdfbdf9b4ccd5959971497
gaurav <gaurav@tallymoney.com>
2022-05-11 16:05:57 +0530

    added logs in middleware [API-1230]

--- middleware/index.js
+++ middleware/index.js
@@ -1,16 +1,39 @@
+const { log } = require("../utils/logger");
+
 module.exports.authGuard = (req, res, next) => {
   try {
     const token = req.headers["accesstoken"];
     if (token != process.env.TALLY_RANDOM_SALT) {
+      log(
+        "Unauthorized User access",
+        {},
+        "info",
+        "_middleware",
+        req.apiGateway.context.awsRequestId
+      );
       res
         .status(401)
-        .send({ message: "UNAUTHORIZED_USER", data: {}, status: 401 });
+        .send({ message: "UNAUTHORIZED_USER_ACCESS", data: {}, status: 401 });
     } else {
+      log(
+        "Got Access for service call",
+        {},
+        "info",
+        "_middleware",
+        req.apiGateway.context.awsRequestId
+      );
       next();
     }
   } catch (err) {
+    log(
+      "Unauthorized User access",
+      {},
+      "error",
+      "_middleware",
+      req.apiGateway.context.awsRequestId
+    );
     res
       .status(401)
-      .send({ message: "UNAUTHORIZED_USER", data: {}, status: 401 });
+      .send({ message: "UNAUTHORIZED_USER_ACCESS", data: {}, status: 401 });
   }
 };
